b9caddcc93c1394b460c029957e121304caeb195
Damian Sikora <damian.sikora@droptica.pl>
2017-08-04 11:40:56 +0200

    tests code

--- tests/tests/_support/Helper/DrupalHelper.php
+++ tests/tests/_support/Helper/DrupalHelper.php
@@ -8,14 +8,13 @@ use Codeception\Lib\Interfaces\Web as WebInterface;
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
 
-class DrupalHelper extends \Codeception\Module
-{
+class DrupalHelper extends \Codeception\Module {
   /**
    * @param $var
    * this will only run if you run codeception with -d
    * Otherwise this is silent
    */
-  public function seeVar($var){
+  public function seeVar($var) {
     $this->debug($var);
   }
 
@@ -47,6 +46,15 @@ class DrupalHelper extends \Codeception\Module
   }
 
   /**
+   * Get WebDriver http auth timeout.
+   *
+   * @return mixed
+   */
+  public function getWebDriverAuthTimeout() {
+      return $this->getModule('WebDriver')->_getConfig('auth_timeout');
+  }
+
+  /**
    * Perform http authentication for WebDriver.
    *
    * @param WebInterface $I
@@ -56,14 +64,17 @@ class DrupalHelper extends \Codeception\Module
     if (method_exists($I, 'executeInSelenium')) {
       $auth_user = $this->getWebDriverAuthUser();
       $auth_pass = $this->getWebDriverAuthPass();
+      $auth_timeout = $this->getWebDriverAuthTimeout();
       $url_auth_part = "{$auth_user}:{$auth_pass}@";
       $url = $this->getWebDriverUrl();
       $url = str_replace('http://', "http://{$url_auth_part}", $url);
       $url = str_replace('https://', "https://{$url_auth_part}", $url);
       $I->seeVar($url);
-      $I->executeInSelenium(function (WebDriver $webdriver) use (&$curr_window, $url) {
-        $webdriver->manage()->timeouts()->pageLoadTimeout(1200);
-        $webdriver->manage()->timeouts()->setScriptTimeout(1200);
+      $I->executeInSelenium(function (WebDriver $webdriver) use (&$curr_window, $auth_timeout, $url) {
+        if ($auth_timeout != NULL) {
+          $webdriver->manage()->timeouts()->pageLoadTimeout($auth_timeout);
+          $webdriver->manage()->timeouts()->setScriptTimeout($auth_timeout);
+        }
         $webdriver->get($url);
       });
     }
